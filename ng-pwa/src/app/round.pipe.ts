5a4720af363134a7f6dd986b043ced7e2e253f4b
Tu Nguyen <nvminhtu@gmail.com>
2018-05-08 23:34:36 +0700

    custom pipe with many parameters

--- ng-pwa/src/app/round.pipe.ts
+++ ng-pwa/src/app/round.pipe.ts
@@ -3,10 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'roundNum' })
 
 export class RoundPipe implements PipeTransform {
-    transform(value: number, toUpper: true):number {
+    transform(value: number, toUpper: true, addTo: number):number {
         if(toUpper) {
-            return Math.ceil(value);
+            return Math.ceil(value + addTo);
         }
-        return Math.floor(value);
+        return Math.floor(value + addTo);
     }
 }
